aedc2b46f8f7acd6104a26dbcf70ad763ccc643c
Sachin <sachin.justsmiling@gmail.com>
2025-06-09 21:32:09 +0530

    Created a branch that should not trigger a workflow

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ function App() {
   return (
     <>
     {/* This is a comment */}
-      <h1>Github Actions</h1>
       <h1>Sachin</h1>
+      <h1>This is line that should not cause a workflow</h1>
+      <h1>Github Actions</h1>
       <h2>Complete course with 2 projects by mukesh</h2>
     </>
   )
