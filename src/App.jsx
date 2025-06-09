827ec47ab3c480cbde9c821bdeaa1e4bba770a09
Sachin <sachin.justsmiling@gmail.com>
2025-06-09 22:53:32 +0530

    Changed the project file to run the workflow

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     {/* This is a comment */}
       <h1>Github Actions</h1>
       <h1>Sachin</h1>
+      <h1>THis will run the workflow</h1>
       <h2>Complete course with 2 projects by mukesh</h2>
     </>
   )
